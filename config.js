5601b6202fad9f389c94ce57f049e2605a6f6e77
PSowmya26 <72786302+PSowmya26@users.noreply.github.com>
2022-08-03 13:37:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDxV9mhJIhjRTuaIiQiJpSS9EhxQ1CsQ4A",
+  authDomain: "storytelling-app-22f62.firebaseapp.com",
+  databaseURL: "https://storytelling-app-22f62-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-22f62",
+  storageBucket: "storytelling-app-22f62.appspot.com",
+  messagingSenderId: "769668943526",
+  appId: "1:769668943526:web:ff8ec417cabaa266232e67"
+};
